1811eb6453bcb4a19f727a3b2e8a1507554a3fe7
IPoorya <84584815+IPoorya@users.noreply.github.com>
2021-05-21 21:31:51 +0430

    Update comment.php

--- comment.php
+++ comment.php
@@ -13,6 +13,8 @@ echo fwrite($file, "$comment");
 fclose($file);
 
 echo "hi! This is a test from 'test' branch";
+echo "the test branch is diffrent from the main, it has two echo commands at the end of the code!";
+echo "now it`s three. bye"
 
 ?>
 
